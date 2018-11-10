b1d3700f1707de65f32f89f72b5505cd36990b63
Amalan Jenicious <amalan.f@shenll.com>
2018-11-10 17:11:40 +0530

    sass support added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-react-shop">
+        <h3>React Shop</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
